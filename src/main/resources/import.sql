c5416198fbdb5e46b2aa9fb0fd3885747ac4ef4b
unknown <anodeconsulting.net@gmail.com>
2018-11-26 11:29:25 -0500

    connect to oracle database using JPA

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(1, 'bmo_support','111@yahoo.com', TO_DATE('2017-02-11', 'yyyy-mm-dd'));
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(2, 'yflow','222@yahoo.com', TO_DATE('2017-02-12', 'yyyy-mm-dd'));
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(3, 'zilap','333@yahoo.com', TO_DATE('2017-02-13', 'yyyy-mm-dd'));
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(4, 'peter','peter1.zhang@bmo.com', TO_DATE('2018-11-13', 'yyyy-mm-dd'));
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(5, 'jsoun','jsoun@bmo.com', TO_DATE('2018-11-13', 'yyyy-mm-dd'));
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(7, 'mkyong','444@yahoo.com', TO_DATE('2017-02-12', 'yyyy-mm-dd'));
